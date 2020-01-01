c14c478e5bb36daa0249309fee660b479bdda820
keltytobin <keltytobin@gmail.com>
2019-12-31 18:07:00 -0700

    canvas drawing app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//===== GLOBAL VARS ========== GLOBAL VARS ========== GLOBAL VARS ========== GLOBAL VARS =====
+const blockPath = new Path2D();
+blockPath.rect(10,10,100,100);
+const SCALE = 0.3;
+const OFFSET = 80;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+//===== HELPER FUNCTIONS ========== HELPER FUNCTIONS ========== HELPER FUNCTIONS ======
+function draw(ctx, location){
+  ctx.save();
+  ctx.scale(SCALE, SCALE);
+  ctx.translate(location.x / SCALE - OFFSET, location.y / SCALE - OFFSET);
+  ctx.fill(blockPath);
+  ctx.restore();
+};
+
+//====== MAIN APP =========== MAIN APP =========== MAIN APP =========== MAIN APP =======
+function App(){
+  const [locations, setLocations] = React.useState([]);
+  const canvasRef = React.useRef(null);
+
+  React.useEffect(()=>{
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect (0, 0, window.innerWidth, window.innerHeight);
+    locations.forEach(location => draw(ctx, location));
+  });
+
+  function handleCanvasClick(e){
+    const newLocation = { x: e.clientX, y: e.clientY };
+    setLocations([...locations, newLocation]);
+  }
+
+  function handleClear () {
+    setLocations([]);
+  }  
+
+  console.log(locations);
+
+  return(
+    <>
+      <button onClick={handleClear}>
+        Clear
+      </button>
+      <canvas
+        ref = {canvasRef}
+        width = {window.innerWidth}
+        height = {window.innerHeight}
+        onClick = {handleCanvasClick}
+      />
+    </>
+  )
+};
+
+export default App
